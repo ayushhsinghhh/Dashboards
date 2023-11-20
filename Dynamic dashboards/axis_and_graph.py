0235a0e6b39295237e61a572968fb2d59bc97c44
ayushhsinghhh <128583659+ayushhsinghhh@users.noreply.github.com>
2023-11-20 21:56:46 +0530

    axis_and_graph.py

--- Dynamic dashboards/axis_and_graph.py
+++ Dynamic dashboards/axis_and_graph.py
@@ -12,9 +12,20 @@ class AxisGraphFrame(ttk.Frame):
         self.has_y_axis = has_y_axis
         self.has_z_axis = has_z_axis
         self.data = data  # Store the data frame
+        master.geometry("400x400")  # Set the width and height as required
 
         ttk.Label(self, text="Select X, Y, and Z Axes for Graph").pack()
 
+        ttk.Label(self, text="Select Graph Color").pack()
+        colors = ["red", "green", "blue", "yellow", "orange", "purple",
+                  "cyan", "magenta", "lime", "pink", "teal", "brown", "black"]
+        self.selected_color = tk.StringVar()
+        self.color_dropdown = ttk.Combobox(self, textvariable=self.selected_color, values=colors)
+        self.color_dropdown.pack()
+
+        self.alpha_frame = ttk.Frame(self)  # Frame to contain the alpha dropdown
+        self.alpha_dropdown = None
+        
         self.x_column_var = tk.StringVar()
         self.y_column_var = tk.StringVar()
         self.z_column_var = tk.StringVar()
@@ -22,6 +33,11 @@ class AxisGraphFrame(ttk.Frame):
         ttk.Label(self, text="X-Axis").pack()
         self.x_column_dropdown = ttk.Combobox(self, textvariable=self.x_column_var)
         self.x_column_dropdown.pack()
+
+        self.add_grid_var = tk.BooleanVar()
+        self.add_grid_var.set(False)
+        self.add_grid_checkbox = ttk.Checkbutton(self, text="Add Grid", variable=self.add_grid_var, command=self.toggle_alpha_dropdown)
+        self.add_grid_checkbox.pack()
 
         if self.has_y_axis:
             ttk.Label(self, text="Y-Axis").pack()
@@ -35,9 +51,22 @@ class AxisGraphFrame(ttk.Frame):
 
         ttk.Button(self, text="Create Graph", command=self.create_graph).pack()
 
-        # Populate comboboxes with column names
         self.populate_column_names()
 
+    def toggle_alpha_dropdown(self):
+        if self.add_grid_var.get():  # Display the alpha dropdown if checkbox is checked
+            if self.alpha_dropdown is None:  # Create the alpha dropdown if it doesn't exist
+                ttk.Label(self.alpha_frame, text="Select Grid Alpha").pack()
+                alpha_values = ["0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "1.0"]
+                self.selected_alpha = tk.StringVar()
+                self.alpha_dropdown = ttk.Combobox(self.alpha_frame, textvariable=self.selected_alpha, values=alpha_values)
+                self.alpha_dropdown.pack()
+                self.alpha_frame.pack()
+        else:  # Hide the alpha dropdown if checkbox is unchecked
+            if self.alpha_dropdown is not None:
+                self.alpha_frame.pack_forget()
+    
+    
     def populate_column_names(self):
         columns = self.data.columns.tolist()
 
@@ -52,6 +81,8 @@ class AxisGraphFrame(ttk.Frame):
         y_column = self.y_column_var.get() if self.has_y_axis else None
         z_column = self.z_column_var.get() if self.has_z_axis else None
         
+        selected_color = self.selected_color.get()
+        
         try:
             if self.graph_type == "3D Scatter" or self.graph_type == "Surface Plot" or self.graph_type == "Wireframe":
                 plt.switch_backend('TkAgg')  # Explicitly set backend to TkAgg
@@ -59,25 +90,25 @@ class AxisGraphFrame(ttk.Frame):
 
             if self.graph_type == "Line Graph":
                 if y_column:
-                    plt.plot(self.data[x_column], self.data[y_column])
+                    plt.plot(self.data[x_column], self.data[y_column], color=selected_color)
                     plt.title("Line Graph")
                 else:
                     messagebox.showerror("Error", "Please select Y-Axis for Line Graph.")
             elif self.graph_type == "Bar Graph":
                 if y_column:
-                    plt.bar(self.data[x_column], self.data[y_column])
+                    plt.bar(self.data[x_column], self.data[y_column], color=selected_color)
                     plt.title("Bar Graph")
                 else:
                     messagebox.showerror("Error", "Please select Y-Axis for Bar Graph.")
             elif self.graph_type == "Histogram":
-                plt.hist(self.data[x_column])
+                plt.hist(self.data[x_column], color=selected_color)
                 plt.title("Histogram")
             elif self.graph_type == "Boxplot":
                 plt.boxplot(self.data[x_column])
                 plt.title("Boxplot")
             elif self.graph_type == "Scatterplot":
                 if y_column:
-                    plt.scatter(self.data[x_column], self.data[y_column])
+                    plt.scatter(self.data[x_column], self.data[y_column], color=selected_color)
                     plt.title("Scatterplot")
                 else:
                     messagebox.showerror("Error", "Please select Y-Axis for Scatterplot.")
@@ -124,6 +155,11 @@ class AxisGraphFrame(ttk.Frame):
                     ax.set_zlabel(z_column)
                 else:
                     messagebox.showerror("Error", "Please select X, Y, and Z-Axis for Wireframe.")
+            
+            if self.add_grid_var.get():  # Check if the checkbox is selected
+                if self.alpha_dropdown is not None:  # Check if alpha dropdown exists
+                    selected_alpha_value = float(self.selected_alpha.get())
+                    plt.grid(True, alpha=selected_alpha_value)
 
             plt.xlabel(x_column)
             if y_column:
